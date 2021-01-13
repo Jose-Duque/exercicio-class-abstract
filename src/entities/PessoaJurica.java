cab10d3cedd024bbe23d91ea153294dbf532c7b2
José Carlos Duque Filho <cj_duque@hotmail.com>
2021-01-13 17:28:19 -0300

    Created class PessoaJurica

--- src/entities/PessoaJurica.java
+++ src/entities/PessoaJurica.java
@@ -1,5 +1,34 @@
 package entities;
 
-public class PessoaJurica {
+public class PessoaJurica extends Contribuinte{
+	
+	private Integer employee;
+	
+	public PessoaJurica() {
+		super();
+	}
+	
+	
+	public PessoaJurica(String name, Double rendaAnual, Integer employee) {
+		super(name, rendaAnual);
+		this.employee = employee;
+	}
+
+	
+	public Integer getEmployee() {
+		return employee;
+	}
+
+
+	public void setEmployee(Integer employee) {
+		this.employee = employee;
+	}
+
+
+	@Override
+	public Double taxesPaid() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
