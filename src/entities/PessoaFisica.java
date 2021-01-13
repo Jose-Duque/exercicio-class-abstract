1800fdc772c28909784b6c88e5492d93d7a6aa4b
José Carlos Duque Filho <cj_duque@hotmail.com>
2021-01-13 17:24:27 -0300

    Created class PessoFisica

--- src/entities/PessoaFisica.java
+++ src/entities/PessoaFisica.java
@@ -1,5 +1,37 @@
 package entities;
 
-public class PessoaFisica {
+public class PessoaFisica extends Contribuinte{
+	
+	private Double health;
+	
+	public PessoaFisica() {
+		super();
+	}
+	
+	
+	
+	public PessoaFisica(String name, Double rendaAnual, Double health) {
+		super(name, rendaAnual);
+		this.health = health;
+	}
+
+	
+
+	public Double getHealth() {
+		return health;
+	}
+
+
+
+	public void setHealth(Double health) {
+		this.health = health;
+	}
+
+
+
+	@Override
+	public Double taxesPaid() {
+		return 0.0;
+	}
 
 }
